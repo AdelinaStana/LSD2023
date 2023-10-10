1069567e5a983398bb20214833ac1d82b454c979
Stana Adelina Diana <stana.adelina.diana@gmail.com>
2023-10-10 20:17:18 +0300

    Update laborator2.py

--- laborator2.py
+++ laborator2.py
@@ -2,8 +2,6 @@
 Python tratează funcțiile ca orice valoare (int, float, ...) pe care ați întâlnit-o până acum.
 Remarcați tipul pe care îl are variabila suma_numere.
 """
-
-
 def suma(x, y):
     return x + y
 
@@ -33,7 +31,8 @@ def aplica_functia(functie, valoare):
 print(f"aplica_functia(increment, 5) = {aplica_functia(increment, 5)}")
 print("aplica_functia(afisare, 5)", end=" -> ")  # pentru a evita ca functia print sa creeze o linie noua in consola,
 # setati parametru end. Valoarea by default a parametrului 'end=' este '\n'
-aplica_functia(afisare, 5)
+
+print(aplica_functia((lambda x: x + 5), 5))
 
 """
 Funcții cu parametrii impliciți (default)
@@ -50,7 +49,7 @@ def test(a, b=0, c=0):
 
 test(1, 2, 3)
 test(3)
-test(3, c=4)
+test(a=3, c=4)
 
 """
 Exercitiu: Scrie o funcție numită 'calculeaza_suprafata' care calculează și returnează aria unui dreptunghi. 
@@ -60,7 +59,11 @@ acesta ar trebui să aibă implicit valoarea 1.
 Formula pentru calcularea ariei unui dreptunghi este lungime * lățime.
 """
 
-# TODO: exercitiu
+def calculeaza_si_afiseaza(lungime, latime=1):
+    return lungime*latime
+
+print(f"calculeaza_si_afiseaza(3,1) = {calculeaza_si_afiseaza(3,1)}")
+print(f"calculeaza_si_afiseaza(3) = {calculeaza_si_afiseaza(3,1)}")
 
 """
 Operatorii sunt funcții. Pentru a avea acces la operatori trebuie sa importam modulul operator. 
@@ -68,7 +71,8 @@ Operatorii sunt funcții. Pentru a avea acces la operatori trebuie sa importam m
 """
 import operator
 
-print(f"operator.mul(5, 6) = {operator.mul(5, 6)}")
+
+print(f"operator.mul(5, 6) = {operator.mul(5, 6)}")  # print(f"operator.mul(5, 6) = {5*6}")
 print(f"operator.add(5, 6) = {operator.add(5, 6)}")
 print(f"tipul functiei operator.add = {type(operator.add)}")
 
@@ -86,7 +90,11 @@ operator (o funcție), a și b. Această funcție ar trebui să aplice operatoru
 să calculeze rezultatul și să-l afișeze pe ecran.
 """
 
-# TODO: exercitiu
+def calculeaza_si_afiseaza(operator, a, b):
+    print(operator(a, b))
+
+calculeaza_si_afiseaza(operator.add, 2,3)
+calculeaza_si_afiseaza(operator.mul, 2,3)
 
 """
 Funcții anonime
@@ -97,6 +105,7 @@ Putem evalua direct fără a fi nevoie să dăm întâi un nume funcției.
 """
 print("lambda:", end=": ")
 print((lambda x: x + 5)(9))
+print((lambda x: x + 5)(8))
 
 lambda1 = lambda x: x + 5
 
@@ -116,6 +125,9 @@ def g(x):
     return x * 2
 
 
+print(f"f(g(2))={f(g(2))}")
+
+
 def comp(f, g):
     return lambda x: f(g(x))
 
@@ -129,17 +141,12 @@ Exercitiu: Scrieți o funcție numită 'compune' care primește o funcție f.
 Funcția compune ar trebui să returneze o nouă funcție care să aplice funcția f de 3 ori.
 """
 
-# TODO: exercitiu
+def comp3(functie):
+    return lambda x: functie(functie(functie(x)))
 
-"""
-Exercițiul 1: valori distincte
+f3 = comp3(f)
 
-Scrieți o funcție cu trei parametri (de același tip oarecare), care returnează câte valori distincte există între 
-argumentele primite (unul, două sau trei) și tipărește, după caz, un mesaj: "toate argumentele sunt distincte/egale"
-sau "argumentele 1 și 2 (resp. 2 și 3, sau 1 și 3) sunt egale". Evitați pe cât posibil duplicarea de cod: 
-pentru porțiuni de cod similare, creați (și apelați) o funcție care conține partea comună și are ca 
-parametri valorile care diferă.
-"""
-# TODO: exercitiu
+print(f"f(f(f(2)))={f3(2)}")
 
-# TODO: TEMA EX 3, 4
+
+# TODO: TEMA EX 1, 3, 4
