2aff8948a0d9560f1b2c428a08fbb3e56a495224
Stana Adelina Diana <stana.adelina.diana@gmail.com>
2023-11-08 16:58:16 +0200

    Update laborator7.py

--- laborator7.py
+++ laborator7.py
@@ -1,5 +1,4 @@
 import functools
-
 """
 Un dicționar constă dintr-o colecție de perechi cheie-valoare. Fiecare pereche cheie-valoare mapează cheia cu valoarea asociată.
 
@@ -35,8 +34,7 @@ Accesarea tuturor valorilor din dictionar se face cu ajutorul values().
 
 Lungimea unui dictionar (numarul de elemente) se obtine cu ajutorul functiei len
 """
-print(
-    f"elemente: {dictionar_capitale.items()}")  # dict_items([('Bucuresti', 'RO'), ('Budapesta', 'Ungaria'), ('Chisinau', 'Moldova')])
+print(f"elemente: {dictionar_capitale.items()}")  # dict_items([('Bucuresti', 'RO'), ('Budapesta', 'Ungaria'), ('Chisinau', 'Moldova')])
 print(f"chei:{dictionar_capitale.keys()}")  # dict_keys(['Bucuresti', 'Budapesta', 'Chisinau'])
 print(f"valori:{dictionar_capitale.values()}")  # dict_values(['RO', 'Ungaria', 'Moldova'])
 print(f"lungimea:{len(dictionar_capitale)}")
@@ -49,9 +47,7 @@ elev_nota = {
 
 print(elev_nota.items())  # dict_items([('Alex', 10), ('Mihai', 9), ('Ioana', 10)])
 
-# Exercitiu: calculati media elevilor
 
-# Varianta1
 def functie_suma(acc, elev):
     nume, nota = elev  # despachetam fiecare tuplu primit ca parametru (exemplu: ('Alex', 10))
     return acc + nota
@@ -61,21 +57,20 @@ def medie_elevi(dictionar):
     suma_note = functools.reduce(functie_suma, dictionar.items(), 0)
     return suma_note / len(dictionar)
 
-# Varianta2
+
 def medie_elevi2(dictionar):
-    suma_note = functools.reduce(lambda suma, elev: suma + elev[1], dictionar.items(), 0)
+    suma_note = functools.reduce(lambda suma, elev:suma + elev[1], dictionar.items(), 0)
     return suma_note / len(dictionar)
 
 
 print(f"Media elevilor {elev_nota} este {medie_elevi(elev_nota)}")
 print(f"Media elevilor {elev_nota} este {medie_elevi2(elev_nota)}")
 
+
 """
 Parcurgerea recursiva a dictionarelor. Pentru parcurgerea recursiva a dictionarelor, convertim dictionarul primit ca
  parametru in 'dict_items', apoi convertim 'dict_items' intr-o lista pe care o sa o parcurgem recursiv.
 """
-
-# Varianta3
 
 def suma_recursiva(dict_list):
     if len(dict_list) > 0:
@@ -92,6 +87,7 @@ def medie_elevi_recursiva(dictionar):
 
 print(f"Media elevilor {elev_nota} este {medie_elevi_recursiva(elev_nota)}")
 
+
 """
 1. Scrieți o funcție care ia o listă de asociere cu perechi de tip (șir, întreg) și creează un dicționar în care 
 fiecare șir e asociat cu suma tuturor valorilor cu care e asociat în listă.
@@ -99,20 +95,21 @@ fiecare șir e asociat cu suma tuturor valorilor cu care e asociat în listă.
 Input: [('a', 7), ('b', 5), ('c', 2), ('a', 3), ('b', 3)]; Output: {'a': 10, 'b': 8, 'c': 2}
 """
 
-def adauga_in_dictionar(dictionar_nou, pereche):
+# TODO: impreuna
+
+def adauga_pereche_in_dictionar(dictionar, pereche):
     cheie, valoare = pereche
-    if cheie in dictionar_nou.keys():
-        dictionar_nou[cheie] += valoare
+    if cheie in dictionar.keys():
+        dictionar[cheie] = dictionar[cheie] + valoare
     else:
-        dictionar_nou[cheie] = valoare
+        dictionar[cheie] = valoare
 
-    return dictionar_nou
+    return dictionar
 
+def convert_pair(lista_perechi):
+    return functools.reduce(lambda dictionar, pereche: adauga_pereche_in_dictionar(dictionar, pereche) , lista_perechi, {} )
 
-def dict_perechi(lista_perechi):
-    return functools.reduce(lambda acc, pereche: adauga_in_dictionar(acc, pereche), lista_perechi, dict())
-
-print(dict_perechi([('a', 7), ('b', 5), ('c', 2), ('a', 3), ('b', 3)]))
+print(convert_pair( [('a', 7), ('b', 5), ('c', 2), ('a', 3), ('b', 3)]))
 
 """
 2. Scrieți o funcție care ia o listă de șiruri de caractere și creează un dicționar în care fiecare șir e asociat cu 
@@ -123,15 +120,16 @@ Input: ["aaa", "bbb", "aabbb"]; Output: {'a': 5, 'b': 6}
 
 # TODO: exercitiu
 
+def string_to_pairs(sirul):
+    return functools.reduce(lambda acc, character: acc + [(character, 1)], list(sirul), [])
 
-def adauga_in_dictionar(dictionar_nou, caracter):
-    if caracter in dictionar_nou.keys():
-        dictionar_nou[caracter] += 1
-    else:
-        dictionar_nou[caracter] = 1
 
-    return dictionar_nou
+def nr_aparitii(lista_siruri):
+    toate_perechile = functools.reduce(lambda lista_perechi, sir: lista_perechi + string_to_pairs(sir), lista_siruri, [])
+    return convert_pair(toate_perechile)
 
+
+print(nr_aparitii(["aaa", "bbb", "aabbb"]))
 
 """
 3. Implementați cu ajutorul lui reduce funcția filter care creează un nou dicționar doar cu perechile din dicționarul 
@@ -141,6 +139,17 @@ Input: dict: {'a': 5, 'b': 7, 'c': 1}; conditie: valoare >= 5; Output: {'a': 5, 
 """
 
 # TODO: exercitiu
+
+def adauga_pereche(pereche, dictionar):
+    cheie, valoare = pereche
+    dictionar[cheie] = valoare
+
+    return dictionar
+
+def filter_dict(dictionar, conditie):
+    return functools.reduce(lambda dict_nou, pereche: adauga_pereche(pereche, dict_nou) if conditie(pereche[1]) else dict_nou, dictionar.items(), {})
+
+print(filter_dict({'a': 5, 'b': 7, 'c': 1}, lambda x: x < 5))
 
 
 # TEMA: ex 4,5,6
