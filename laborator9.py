dd04efc6374ecdbb5b76059b8decae4ed132cb1d
Stana Adelina Diana <stana.adelina.diana@gmail.com>
2023-11-22 07:52:12 +0200

    Add files via upload

---
+++ laborator9.py
@@ -0,0 +1,30 @@
+import functools
+
+"""
+MULTIMI
+Scrieți o funcție care primește o listă de șiruri de caractere și returnează o mulțime conținând literele unice din toate șirurile.
+
+Input: ['apple', 'banana', 'cherry']; Output: {'a', 'b', 'c', 'e', 'h', 'l', 'n', 'p', 'r', 'y'}
+"""
+
+# TODO: exercitiu
+
+"""
+DICTIONARE
+Scrieți o funcție care primește o funcție și un dicționar și returnează maximul valorilor funcției pentru toate intrările dicționarului.
+Funcția-parametru are ca argumente cheia și valoarea unei intrări, și poate returna valori arbitrare. 
+Folosiți reduce pentru parcurgere, și max (definită implicit pentru orice tip) pentru a 
+compara valorile returnate de funcția parametru.
+"""
+# TODO: exercitiu
+
+
+"""
+ARBORI
+
+Definiti un arbore binar cu valori intregi pozitive, apoi scrieti o functie care:
+ A. calculeaza suma tuturor nodurilor din arbore.
+ B. calculeaza maximul tuturor nodurilor din arbore.
+"""
+
+# TODO: exercitiu
