4b4ab542570e20c108e95ceab4c11a48cb513a9c
Stana Adelina Diana <stana.adelina.diana@gmail.com>
2023-12-06 07:20:15 +0200

    Update laborator9.py

--- laborator9.py
+++ laborator9.py
@@ -1,4 +1,3 @@
-import functools
 
 """
 MULTIMI
@@ -6,8 +5,24 @@ Scrieți o funcție care primește o listă de șiruri de caractere și returnea
 
 Input: ['apple', 'banana', 'cherry']; Output: {'a', 'b', 'c', 'e', 'h', 'l', 'n', 'p', 'r', 'y'}
 """
+import functools
+
 
 # TODO: exercitiu
+
+def multime_caractere(lista_siruri):
+    return functools.reduce(lambda multime, sir: multime | set(sir), lista_siruri, set())
+
+def sir_to_mul(sir):
+    if len(sir) > 0:
+        return set(sir[0]) | sir_to_mul(sir[1:])
+    else:
+        return set()
+
+def multime_caractere2(lista_siruri):
+    return functools.reduce(lambda multime, sir: multime | sir_to_mul(sir), lista_siruri, set())
+
+print(multime_caractere2(['apple', 'banana', 'cherry']))
 
 """
 DICTIONARE
@@ -19,6 +34,19 @@ compara valorile returnate de funcția parametru.
 # TODO: exercitiu
 
 
+def maxim_dictionar(functie, dictionar):
+    first = list(dictionar.items())[0]
+    first = functie(first)
+    return functools.reduce(lambda maxim, pereche: max(maxim, functie(pereche)), dictionar.items(), first)
+
+
+def func(pereche):
+    cheie, valoare = pereche
+    return valoare * 2
+
+
+print(maxim_dictionar(func, {'a': 5, 'b': 7, 'c': 1}))
+
 """
 ARBORI
 
@@ -27,4 +55,33 @@ Definiti un arbore binar cu valori intregi pozitive, apoi scrieti o functie care
  B. calculeaza maximul tuturor nodurilor din arbore.
 """
 
+
+cheie = "cheie"
+stanga = "stanga"
+dreapta = "dreapta"
+
+
+nod1 = {cheie: 1, stanga: None, dreapta: None}  # frunza
+nod7 = {cheie: 7, stanga: None, dreapta: None}  # frunza
+nod4 = {cheie: 4, stanga: None, "dreapta": None}  # frunza
+nod5 = {cheie: 15, stanga: nod4, dreapta: nod7}
+radacina = {cheie: 2, stanga: nod1, dreapta: nod5}
+
 # TODO: exercitiu
+
+def suma(arbore):
+    if arbore is not None:
+        return arbore[cheie] + suma(arbore[stanga]) + suma(arbore[dreapta])
+    else:
+        return 0
+
+print(suma(radacina))
+
+
+def maxim(arbore):
+    if arbore is not None:
+        return max(arbore[cheie], maxim(arbore[stanga]), maxim(arbore[dreapta]))
+    else:
+        return 0
+
+print(maxim(radacina))
